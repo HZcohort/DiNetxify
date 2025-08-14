95feaf4648f58b2eeff1953208aa02435c5e7765
bovin <haowenliu81@gmail.com>
2025-08-14 14:29:44 +0800

    add the codes of case study, workflows.yml, delete some useless files.

---
+++ case_study/case_study_1/RPCN/codes.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import sys
+import time
+sys.path.append(r'/public/home/houcan/project/case_study/DiNetxify-main/src')
+import DiNetxify as dnt
+n_cores = 40
+min_interval_days = 30
+max_interval_days = 5*365
+
+path_case = r'/public/home/houcan/project/case_study/data'
+path_result = r'/public/home/houcan/project/case_study/result/ltl_rpcn'
+
+if __name__ == "__main__":
+    data = dnt.DiseaseNetworkData(study_design='cohort',phecode_level=2,
+                                phecode_version='1.3a')
+    time1 = time.time()
+    data.phenotype_data(rf'{path_case}/LTL.csv',
+                        column_names={'Participant ID':'eid',
+                                    'Sex': 'sex',
+                                    'Index date': 'date_attend',
+                                    'End date': 'date_end',
+                                    'Exposure': 'ltl'},
+                        covariates=['age','social_cate','BMI','income','diet','smoking','drinking','physical'])
+    data.merge_medical_records(rf'{path_case}/icd10.csv',
+                            diagnosis_code='ICD-10-WHO',column_names={'Participant ID': 'eid',
+                                                                        'Diagnosis code': 'diag_icd10',
+                                                                        'Date of diagnosis': 'dia_date'})
+    data.merge_medical_records(rf'{path_case}/icd9.csv',
+                            diagnosis_code='ICD-9-WHO',column_names={'Participant ID': 'eid',
+                                                                        'Diagnosis code': 'diag_icd9',
+                                                                        'Date of diagnosis': 'dia_date'})
+    time2 = time.time()
+    print(f"Data harmonization cost {(time2-time1)/60} min")
+
+    #step 1
+    phewas_result = dnt.phewas(
+        data=data,                                             # DiseaseNetworkData object
+        n_threshold=500,                                       # Minimum proportion of cases to include
+        n_process=n_cores,                                     # Number of parallel processes
+        system_exl=[                                           # Phecode systems to exclude
+            'pregnancy complications','congenital anomalies', 'symptoms', 'others', 'injuries & poisonings'],
+        phecode_exl=[681.3, 681.5],
+        covariates=['age', 'sex', 'social_cate','BMI','income','diet','smoking','drinking','physical'],
+        lifelines_disable=True,                                # Disable lifelines for faster computation
+        log_file=path_result+'/phewas.log',                # Path to log file
+        correction='none'
+    )
+    phewas_result.to_csv(f'{path_result}/phewas.csv',index=False)
+    
+    #step 2
+    phewas_result = pd.read_csv(rf'{path_result}/phewas.csv')
+    phewas_result = phewas_result[phewas_result['phewas_coef'] > 0]
+    phewas_result = dnt.phewas_multipletests(phewas_result,correction='fdr_bh')
+    phewas_result.to_csv(f'{path_result}/phewas_multiple.csv',index=False)
+    print('Phewas: ',len(phewas_result[phewas_result['phewas_p_significance']==True]))
+
+    data.disease_pair(phewas_result=phewas_result,min_interval_days=min_interval_days,max_interval_days=max_interval_days,force=True)
+    #data.save(rf'{path_processed}/whole_60_withtra')
+
+    #step 3
+    com_strength_result = dnt.comorbidity_strength(
+        data=data,                                     # DiseaseNetworkData object
+        n_threshold=50,                   # Minimum proportion for comorbidity
+        n_process=n_cores,                                   # Number of parallel processes
+        log_file=path_result+'/comorbidity.log'          # Path to log file
+    )
+    com_strength_result.to_csv(f'{path_result}/comorbidity_strength.csv',index=False)
+
+    com_strength_result = pd.read_csv(rf'{path_result}/comorbidity_strength.csv')
+    com_strength_result = com_strength_result[(com_strength_result['phi'] > 0) & (com_strength_result['RR'] > 1)]
+    com_strength_result = dnt.comorbidity_strength_multipletests(df=com_strength_result,
+                                                                 correction_phi='fdr_bh',correction_RR='fdr_bh',
+                                                                 cutoff_phi=0.05,cutoff_RR=0.05)
+    print('Comorbidity strength: ',len(com_strength_result[(com_strength_result['RR_p_significance']==True) & 
+                                                       (com_strength_result['phi_p_significance']==True)]))
+
+    #step 4
+    comorbidity_result = dnt.comorbidity_network(
+        data=data,                                       # DiseaseNetworkData object
+        comorbidity_strength_result=com_strength_result, # Comorbidity strength results
+        n_process=n_cores,                                   # Number of parallel processes
+        covariates=['age','sex','social_cate','BMI','income','diet','smoking','drinking','physical'],  # Covariates to adjust for
+        method='RPCN',                                   # Analysis method ('CN', 'PCN_PCA', 'RPCN')
+        log_file=path_result+'/uncond_logistic.log'         # Path to log file
+    )
+    comorbidity_result = dnt.comorbidity_multipletests(df=comorbidity_result,correction='fdr_bh',cutoff=0.05)
+    print('Comorbidity network: ',len(comorbidity_result[comorbidity_result['comorbidity_p_significance']==True]))
+    comorbidity_result.to_csv(f'{path_result}/comorbidity_RPCN.csv',index=False)
+    
+    #step 5
+    binomial_result = dnt.binomial_test(
+    data=data,                                        # DiseaseNetworkData object
+    comorbidity_strength_result=com_strength_result,  # Comorbidity strength results
+    comorbidity_network_result=comorbidity_result,    # Comorbidity network results
+    n_process=1,                                      # Number of CPU cores (1 to disable multiprocessing)
+    enforce_temporal_order=True,                      # Enforce temporal order in testing
+    log_file=path_result+'/binomial.log'              # Path to log file
+    )
+    binomial_result = dnt.binomial_multipletests(df=binomial_result, correction='fdr_bh', cutoff=0.05)
+    print('Binomial: ',len(binomial_result[binomial_result['binomial_p_significance']==True]))
+    binomial_result.to_csv(f'{path_result}/binomial.csv',index=False)
+
+    #step 6
+    trajectory_result = dnt.disease_trajectory(
+        data=data,                                       # DiseaseNetworkData object
+        comorbidity_strength_result=com_strength_result, # Comorbidity strength results
+        binomial_test_result=binomial_result,           # Binomial test results
+        method='RPCN',                                   # Trajectory analysis method ('CN', 'PCN_PCA', 'RPCN')
+        n_process=n_cores,                                     # Number of parallel processes
+        matching_var_dict={'sex': 'exact', 'age': 2, 'social_cate': 'exact'},    # Matching variables and criteria
+        matching_n=5,                                    # Number of matched controls per case
+        enforce_time_interval=True,                     # Enforce time interval in trajectory analysis
+        covariates=['age','BMI','income','diet','smoking','drinking','physical'],  # Covariates to adjust for
+        log_file=path_result+'/cond_logistic.log',            # Path to log file
+        global_sampling=True)
+    trajectory_result = dnt.trajectory_multipletests(df=trajectory_result,correction='fdr_bh',cutoff=0.05)
+    print('Trajectory analysis: ',len(trajectory_result[trajectory_result['trajectory_p_significance']==True]))
+    trajectory_result.to_csv(f'{path_result}/trajectory_RPCN_match5.csv',index=False)
+
